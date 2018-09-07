e0033ae7c948b1db71c31e0dc4c48873b00e2706
Marvin <hofer@informatik.uni-leipzig>
2018-09-07 16:27:32 +0200

    tsv format extended

--- generate-markdown.sh
+++ generate-markdown.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 cd "$( dirname "${BASH_SOURCE[0]}")"
 while IFS='' read -r line || [[ -n "$line" ]]; do
-	entryname=${line%%	*}
-	repourl=${line##*	}
-	readmeurl=`echo $repourl/master/README.md | sed s/github.com/raw.githubusercontent.com/g`
+#	entryname=${line%%	*}
+    entryname=`echo "$line" | cut -d$'\t' -f1`
+#	repourl=${line##*	}
+    repourl=`echo "$line" | cut -d$'\t' -f2`
+#	readmeurl=`echo $repourl/master/README.md | sed s/github.com/raw.githubusercontent.com/g`
+	readmeurl=`echo "$line" | cut -d$'\t' -f3`
 	echo -e "---\nlayout: readme\ntitle: \"$entryname\"\npermalink: \"$entryname\"\n---\n" > "content/$entryname.md"
 	echo 'This page was included from <a target="_blank" href="'$repourl'">'$repourl'</a>.' >> "content/$entryname.md"
 	curl -s $readmeurl >> "content/$entryname.md"
