b835aa5404b7a8ea7b46d84a3efb697e87c9a96c
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-01-16 13:32:08 +0100

    changed csv order

--- generate-markdown.sh
+++ generate-markdown.sh
@@ -2,33 +2,38 @@
 cd "$( dirname "${BASH_SOURCE[0]}")"
 rank=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
-#	entryname=${line%%	*}
+
+	# get the table columns
 	entryname=`echo "$line" | cut -d$'\t' -f1`
-	filename=`echo "$entryname" | sed "s/ /_/g"`
-#	repourl=${line##*	}
-	repourl=`echo "$line" | cut -d$'\t' -f2`
+	parent=`echo "$line" | cut -d$'\t' -f2`
+	repourl=`echo "$line" | cut -d$'\t' -f3`
+	readmeurl=`echo "$line" | cut -d$'\t' -f4`
+	
+
 
 	includetext=""
-#	readmeurl=`echo $repourl/master/README.md | sed s/github.com/raw.githubusercontent.com/g`
-	readmeurl=`echo "$line" | cut -d$'\t' -f3`
+	permalink=`echo "$entryname" | sed "s/ /_/g"`
 
-	parent=`echo "$line" | cut -d$'\t' -f4`
 
+	# generating the header of each page
     if [ -n "$repourl" ]
     then
         includetext=`echo 'This page was included from <a target="_blank" href="'$repourl'">'$repourl'</a>.'`
     fi
+    
+	# generate jekyll header
+    
 	if [ -z "$parent" ]
 	then
-		#echo -e "---\nlayout: readme\ntitle: \"$entryname\"\npermalink: \"$filename\"\nrank: \"$(echo -n 0$rank)\"\n---\n" > "content/$entryname.md"
-		echo -e "---\nlayout: readme\ntitle: \"$entryname\"\npermalink: \"$filename\"\nrank: $rank\n---\n" > "content/$entryname.md"
-		echo -e "$includetext\n" >> "content/$entryname.md"
-		curl -s "$readmeurl" >> "content/$entryname.md"
+		echo -e "---\nlayout: readme\ntitle: \"$entryname\"\npermalink: \"$permalink\"\nrank: $rank\n---\n" > "content/$entryname.md"
 	else
-		#echo -e "---\nlayout: subpage\ntitle: \"$entryname\"\npermalink: \"$filename\"\nparent: \"$parent\"\nrank: \"$(echo -n 0$rank)\"\n---\n" > "content/$entryname.md"
-		echo -e "---\nlayout: subpage\ntitle: \"$entryname\"\npermalink: \"$filename\"\nparent: \"$parent\"\nrank: $rank\n---\n" > "content/$entryname.md"
-		echo -e "$includetext\n" >> "content/$entryname.md"
-		curl -s "$readmeurl" >> "content/$entryname.md"
+		echo -e "---\nlayout: subpage\ntitle: \"$entryname\"\npermalink: \"$permalink\"\nparent: \"$parent\"\nrank: $rank\n---\n" > "content/$entryname.md"
 	fi
+	
+	# filling with header and page content
+	echo -e "$includetext\n" >> "content/$entryname.md"
+	curl -s "$readmeurl" >> "content/$entryname.md"
+	
+	
 	((rank+=1))
 done < <(tail -n+2 readme-list.tsv)
