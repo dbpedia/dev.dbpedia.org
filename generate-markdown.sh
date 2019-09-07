69751b7d81055d937d348becd1a70970a39ef66b
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2019-09-07 10:08:24 +0200

    about

--- generate-markdown.sh
+++ generate-markdown.sh
@@ -23,7 +23,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	# generating the header of each page
     if [ -n "$repourl" ]
     then
-        includetext=`echo 'This page was included from <a target="_blank" href="'$repourl'">'$repourl'</a>.'`
+        includetext=`echo '**[Edit this page]($repourl)**'`
     fi
     
 	# generate jekyll header
