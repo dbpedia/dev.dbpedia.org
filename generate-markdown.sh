9bea27884b309eaa25e4258828855b2c3b89de28
Sebastian Hellmann <hellmann@informatik.uni-leipzig.de>
2020-08-12 11:06:50 +0200

    Update generate-markdown.sh

--- generate-markdown.sh
+++ generate-markdown.sh
@@ -23,7 +23,7 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	# generating the header of each page
     if [ -n "$repourl" ]
     then
-        includetext=`echo '**[Edit this page on Github]('$repourl')**'`
+        includetext=`echo '**[Edit this page with Git]('$repourl')**'`
     fi
     
 	# generate jekyll header
